2ccab4ee80c49b5f532babfd00560e09445b148f
zafar-m3n <thezafar.m3n@gmail.com>
2025-07-02 17:49:19 +0530

    User Model

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    full_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password_hash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("client", "admin"),
+      defaultValue: "client",
+    },
+    email_verified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    verification_token: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    reset_token: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    reset_token_expiry: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+module.exports = User;
