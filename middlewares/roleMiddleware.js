d5ecb7a6e2654cd375b9cd5fda0a2905f2664892
zafar-m3n <thezafar.m3n@gmail.com>
2025-07-03 18:12:47 +0530

    Role Middleware

---
+++ middlewares/roleMiddleware.js
@@ -0,0 +1,10 @@
+const authorizeRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access denied. Insufficient permissions." });
+    }
+    next();
+  };
+};
+
+module.exports = authorizeRoles;
