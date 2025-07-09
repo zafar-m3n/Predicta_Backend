07dc6642c4b7a78840c6936577a17f4d0a7aa600
zafar-m3n <thezafar.m3n@gmail.com>
2025-07-09 15:13:18 +0530

    Route imports

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getWalletBalance, getDepositHistory } = require("../../controllers/walletController");
-const authenticate = require("../../middlewares/authMiddleware");
-const authorizeRoles = require("../../middlewares/roleMiddleware");
+const { getWalletBalance, getDepositHistory } = require("../controllers/walletController");
+const authenticate = require("../middlewares/authMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware");
 
 // Protect all wallet routes: must be authenticated as client
 router.use(authenticate);
